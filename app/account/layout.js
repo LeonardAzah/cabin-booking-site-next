43ca17b2ac65c33a10f68178783d731fb5997943
LeonardAzah <isongleonard@gmail.com>
2025-09-08 17:05:10 +0100

    build site add SEO, and cashing

---
+++ app/account/layout.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+import SideNavigation from "../_components/SideNavigation";
+
+const AccountLayout = ({ children }) => {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12 ">
+      <SideNavigation />
+      <div className="py-1">{children}</div>
+    </div>
+  );
+};
+
+export default AccountLayout;
---
+++ app/account/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Jonas
+      </h2>
+    </div>
+  );
+};
+
+export default page;
